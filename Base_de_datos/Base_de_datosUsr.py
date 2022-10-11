4c62492e185eb895840c55416acc998eac841a2c
Manuel - Pc Aux <manuelcruzg78@gmail.com>
2022-10-11 10:28:23 -0500

    Se agregan funciones al metodo para que este pueda escribir informacion y leer. Faltan funciones de filtracion de datos

--- Base_de_datos/Base_de_datosUsr.py
+++ Base_de_datos/Base_de_datosUsr.py
@@ -8,7 +8,7 @@ def CrearBase():
     
 def CrarTabla():
     conn = sqlite3.connect('Miembros.db')
-    cursor = conn.Cursor()
+    cursor = conn.cursor()
     cursor.execut(
         """CREATE TABLE IF NOT EXISTS usuarios (
             rol TEXT,
@@ -22,8 +22,39 @@ def CrarTabla():
     conn.commit()
     conn.close()     
     
-def InstertarUsuario(Usuario):
-    conn = sqlite3.connect()
+def InsertarUsuario(usr):
+    conn = sqlite3.connect('Miembros.db')
     cursor = conn.cursor()
-    instruccion = "INSERT"
+    añadir = f"INSERT INTO usuarios VALUES ('{usr.rol}', '{usr.nombre}', '{usr.salario}','{usr.id}', '{usr.usuario}', '{usr.contraseña}')"
+    cursor.execute(añadir)
+    conn.commit()
+    conn.close()
+    
+def InsertarUsuarios(usr_lista):
+    conn = sqlite3.connect('Miembros.db')
+    cursor = conn.cursor()
+    añadir = f"INSERT INTO usuarios VALUES (?, ?, ?, ?, ?, ?)"
+    cursor.executemany(añadir, usr_lista)
+    conn.commit()
+    conn.close()
+
+def LeerBase():
+    conn = sqlite3.connect('Miembros.db')
+    cursor = conn.cursor()
+    leer = f"SELECT * FROM usuarios"
+    cursor.execute(leer)
+    datos = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    return datos
+    
+def LeerBaseOrdenada(campo):
+    conn = sqlite3.connect('Miembros.db')
+    cursor = conn.cursor()
+    leer_ordenado = f"SELECT * FROM usuarios ORDERED BY {campo}"
+    cursor.execute(leer_ordenado)
+    datos = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    return datos
     
