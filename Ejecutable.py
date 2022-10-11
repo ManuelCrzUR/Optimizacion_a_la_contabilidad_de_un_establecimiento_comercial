09507c35b15b597b7388e76593d6b388b9ac5b5a
LordMatalas <77035863+LordMatalas@users.noreply.github.com>
2022-10-11 10:43:24 -0500

    Modificaciones leves

---
+++ Ejecutable.py
@@ -0,0 +1,4 @@
+'''
+Este archivo tiene como finalidad ser el menú principal para el usuario. 
+Será el archivo que enlace todos los documentos, módulos y clases, por este motivo se llama Ejecutable.py 
+'''
